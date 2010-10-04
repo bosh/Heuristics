2414c02f4e0f9f121f93105200a1d540c52d1d5c
Bosh <ohwaitnvm a gmail d com>
2010-10-04 04:36:48 -0400

    bang bang bang

--- salesman.rb
+++ salesman.rb
@@ -14,10 +14,13 @@ class PointSet
 	attr_accessor :points_by_x, :points_by_y, :points_by_z, :points
 	def initialize
 		@points = {}
-		#read in all points into :points
-		@points_by_x = generate_points_by_x
-		@points_by_y = generate_points_by_y
-		@points_by_z = generate_points_by_z
+		File.open("salesman_data.txt","r") do |f|
+			f.each_line{|line| line = line.split(" "); @points[line[0].to_i] = Point.new(line[0].to_i, line[1].to_i, line[2].to_i, line[3].to_i) }
+		end
+		data = @points.map(&:to_data)
+		# @points_by_x = data.sort_by{|(num,x,y,z)| [x, y, z, num]}
+		# @points_by_y = data.sort_by{|(num,x,y,z)| [y, x, z, num]}
+		# @points_by_z = data.sort_by{|(num,x,y,z)| [z, x, y, num]}
 	end
 end
 
@@ -25,11 +28,15 @@ class PathSet
 	attr_accessor :paths
 	def initialize(points)
 		@paths = create_mst(points)
-		@optimize until time_up?
+		optimize! until time_up?
 	end
 	def create_mst(points)
-		@paths = {}
-		#djikstras here
+		connections = []
+		#djikstras here into connections
+		connections
+	end
+	def optimize!
+		#HNNNNG!
 	end
 	def to_s
 		@paths.join(",")
@@ -39,10 +46,13 @@ end
 class Point
 	attr_accessor :number, :x, :y, :z
 	def initialize(no, x, y, z)
-		@number = number
+		@number = no
 		@x = x
 		@y = y
 		@z = z
+	end
+	def to_data
+		[@number, @x, @y, @z]
 	end
 	def distance_to(point) #Calulates the distance to another point. Could use lookup
 		coords = [self, point].map{|p| [p.x, p.y, p.z]}
@@ -53,14 +63,18 @@ class Point
 end
 
 class Path
-	attr_accessor :endpoints, :distance, :uid
+	attr_accessor :endpoints, :distance, :uid, :xuid
 	def initialize(point1, point2)
 		@endpoints = [point1, point2]
-		@uid = "#{@endpoints.sort(&:number).map(&:number).join(',')}" #may want to generate two uids and save both...
+		@uid = @endpoints.map(&:number)
+		@xuid = @uid.reverse
 		@distance = find_distance
 	end
 	def find_distance #Calculates the distance and saves it to a lookup table
-		$paths[@uid] ||= @endpoints.first.distance_to @endpoints.last #may find it useful to generate both uids and save into both
+		$paths[@uid.first] ||= []
+		$paths[@xuid.first] ||= []
+		$paths[@uid.first][@uid.last] ||= @endpoints.first.distance_to @endpoints.last
+		$paths[@xuid.first][@xuid.last] ||= @endpoints.first.distance_to @endpoints.last
 	end
 	def connects_to?(point)
 		@endpoints.include? point
@@ -73,7 +87,7 @@ end
 
 $start_time = Time.now
 $seconds_to_run = 10
-$paths = {}
+$paths = [] # Distance calculation store
 results = FullPath.new
 puts "\t\t**Algorithm Stopped**\nTime Elapsed: #{Time.now - $start_time} seconds\n"
 puts results
