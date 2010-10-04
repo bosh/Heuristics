b4d8a54a333ab1f23968864c4bdbaf6c5529f6fe
Bosh <ohwaitnvm a gmail d com>
2010-10-04 15:08:29 -0400

    chug chug chug

--- salesman.rb
+++ salesman.rb
@@ -1,6 +1,6 @@
 class FullPath
 	attr_accessor :points, :paths, :total_solution_distance
-	def intialize
+	def initialize
 		@points = PointSet.new
 		@paths = PathSet.new(@points)
 		@total_solution_distance = @paths.calculate_distance
@@ -13,30 +13,35 @@ end
 class PointSet
 	attr_accessor :points_by_x, :points_by_y, :points_by_z, :points
 	def initialize
-		@points = {}
+		@points = []
 		File.open("salesman_data.txt","r") do |f|
 			f.each_line{|line| line = line.split(" "); @points[line[0].to_i] = Point.new(line[0].to_i, line[1].to_i, line[2].to_i, line[3].to_i) }
 		end
-		data = @points.map(&:to_data)
-		# @points_by_x = data.sort_by{|(num,x,y,z)| [x, y, z, num]}
-		# @points_by_y = data.sort_by{|(num,x,y,z)| [y, x, z, num]}
-		# @points_by_z = data.sort_by{|(num,x,y,z)| [z, x, y, num]}
+		data = @points.reject(&:nil?).map(&:to_data)
+		@points_by_x = data.sort_by{|(num,x,y,z)| [x, y, z, num]}
+		@points_by_y = data.sort_by{|(num,x,y,z)| [y, x, z, num]}
+		@points_by_z = data.sort_by{|(num,x,y,z)| [z, x, y, num]}
 	end
 end
 
 class PathSet
 	attr_accessor :paths
-	def initialize(points)
+	def initialize(pointset)
+		points = pointset.points
+		(2...points.length).each {|i| $paths[i] ||= []; (1...i).each {|j| $paths[j] ||= []; $paths[i][j] = $paths[j][i] = points[i].distance_to(points[j])}}
 		@paths = create_mst(points)
 		optimize! until time_up?
 	end
 	def create_mst(points)
 		connections = []
-		#djikstras here into connections
+		#prims here into connections
 		connections
 	end
 	def optimize!
 		#HNNNNG!
+	end
+	def calculate_distance
+		@paths.inject{|sum,path| sum += path.distance}
 	end
 	def to_s
 		@paths.join(",")
@@ -71,22 +76,17 @@ class Path
 		@distance = find_distance
 	end
 	def find_distance #Calculates the distance and saves it to a lookup table
-		$paths[@uid.first] ||= []
-		$paths[@xuid.first] ||= []
-		$paths[@uid.first][@uid.last] ||= @endpoints.first.distance_to @endpoints.last
-		$paths[@xuid.first][@xuid.last] ||= @endpoints.first.distance_to @endpoints.last
+		$paths[@uid.first][@uid.last]
 	end
 	def connects_to?(point)
 		@endpoints.include? point
 	end
 end
 
-def time_up?
-	Time.now - $start_time > $seconds_to_run
-end
+def time_up?; Time.now - $start_time > $seconds_to_run end
 
 $start_time = Time.now
-$seconds_to_run = 10
+$seconds_to_run = 4
 $paths = [] # Distance calculation store
 results = FullPath.new
 puts "\t\t**Algorithm Stopped**\nTime Elapsed: #{Time.now - $start_time} seconds\n"
