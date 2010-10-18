38b5737f82b42422aca2ffd0d97a8574f00154db
bosh <ohwaitnvm@gmail.com>
2010-10-18 04:00:19 -0400

    added pattern matching and data population, added all server message headers

--- NoTipping/Client.java
+++ NoTipping/Client.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.*;
+import java.util.regex.*;
 
 public class Client {
     public static void main(String[] args) throws Exception {
@@ -24,15 +25,40 @@ public class Client {
         String fromUser;
         out.println(clientName);
 
+        boolean[] weights_self = new boolean[10];
+        boolean[] weights_opp = new boolean[10];
+
         while ((fromServer = in.readLine()) != null) {
-            String response = "";
+            if (fromServer.equals("Bye")) { break; }
             System.out.println("Server: " + fromServer);
-            if (fromServer.equals("Bye")) {
-                break;
-            } else if (fromServer.startsWith("ADD")) {
+
+            String response = "";
+            String[] matches = Pattern.compile("[|]").split(fromServer);
+            String[] placements = Pattern.compile(" ").split(matches[1]);
+            int[] weights = new int[placements.length];
+            int[] positions = new int[placements.length];
+            for(int i = 0; i < placements.length; i++) {
+                String[] placement = Pattern.compile(",").split(placements[i]);
+                int weight = Integer.parseInt(placement[0].trim());
+                int position = Integer.parseInt(placement[1].trim());
+                weights[i] = weight;
+                positions[i] = position;
+            }
+            
+            if (fromServer.startsWith("ADD")) {
+                // response = nextAddMove(weights, positions, weights_self, weights_opp);
                 out.println(response);
             } else if (fromServer.startsWith("REMOVE")) {
-                out.println(response);
+                // response = nextRemoveMove(weights, positions);
+               out.println(response);
+            } else if (fromServer.startsWith("REJECT")) {
+            } else if (fromServer.startsWith("ACCEPT")) {
+            } else if (fromServer.startsWith("TIP")) {
+                break;
+            } else if (fromServer.startsWith("WIN")) {
+                break;
+            } else if (fromServer.startsWith("TIMEOUT")) {
+                break;
             }
         }
         out.close();
