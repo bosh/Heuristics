403441838ed0a403aded867ea2ddbd1c8ea20329
bosh <ohwaitnvm@gmail.com>
2010-11-08 15:47:01 -0500

    wip commit: completing polygons

--- Voronoi/polygon.rb
+++ Voronoi/polygon.rb
@@ -2,6 +2,43 @@ class Polygon
 	attr_accessor :center, :vertices
 	def initialize(center, lines)
 		@center = center
+		lines.map! do |line|
+			intersections = []
+			lines.each{|other| intersections << Intersection.new(line, other) unless line == other }
+			{:line => line, :intersections => intersections}
+		end
+
+		closest = {:distance => 9999, :intersection => nil}
+		lines.each do |line|
+			line[:intersections].each do |inter|
+				if @center.distance_to(inter) < closest[:distance]
+					closest[:intersection] = inter
+					closest[:distance] = @center.distance_to(inter)
+				end
+			end
+		end
+
+		next_closest = {:distance => 9999, :intersection => nil}
+		closest[:intersection].lines.each do |line|
+			line_intersections = lines[lines.index(lines.select{|l| l[:line] == line}.first)][:intersections]
+			line_intersections.each do |inter|
+				if inter != closest[:intersection] && @center.distance_to(inter) < next_closest[:distance]
+					next_closest[:intersection] = inter
+					next_closest[:distance] = @center.distance_to(inter)
+				end
+			end
+		end
+
+		poly = [closest[:intersection], second_closest[:intersection]]
+		unshift_line = poly.first.lines - poly[1].lines #should end up with only one
+		push_line = poly.last.lines - poly[-2].lines #should also end up with only one
+		open_lines = [unshift_line, push_line]
+
+
+		get_next_closest from the two lines
+		repeat until two open lines are ==
+		create vertices from the intersections, put them in @vertices
+
 		#TODO create polygon from set of lines
 		#closest_intersection = nil
 		#intersections = lines.map{|l| l.intersections(lines)}
@@ -14,12 +51,6 @@ class Polygon
 		#    end
 		#  end
 		#end
-		#after getting the closest, get the next closest point on said bisector
-		#then get the bisectors associated with those two vertices
-		#find their closest, non-outside intersection
-		#get the new pair of unclosed/unpaired bisector/segments
-		#repeat until both unclosed are from the same bisector parent (ie the close each other)
-		#dump out the collected set as a polygon
 	end
 
 	def area
@@ -28,7 +59,7 @@ class Polygon
 		(2...vertices.length).each do |i|
 			p2 = vertices[i-1]
 			p3 = vertices[i]
-			area += ((p1.x*(p2.y - p3.y) + p2.x*(p3.y-p1.y) + p3.x*(p1.y - p2.y))/2).abs
+			area += ((p1.x*(p2.y - p3.y) + p2.x*(p3.y-p1.y) + p3.x*(p1.y - p2.y))/2.0).abs
 		end
 	end
 end
