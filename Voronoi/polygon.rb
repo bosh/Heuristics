13f5956c29cef1bc345e47bfe8d33dd0611d5203
bosh <ohwaitnvm@gmail.com>
2010-11-08 05:17:00 -0500

    notes on turning lines into a polygon

--- Voronoi/polygon.rb
+++ Voronoi/polygon.rb
@@ -3,6 +3,23 @@ class Polygon
 	def initialize(center, lines)
 		@center = center
 		#TODO create polygon from set of lines
+		#closest_intersection = nil
+		#intersections = lines.map{|l| l.intersections(lines)}
+		#intersections.each_with_index do |inter, i|
+		#  inter.each do |p|
+		#    dist = @center.distance_to(p)
+		#    if dist < closest_intersection.distance_to(@center)
+		#		closest_intersection = p
+		#       closest_segment = i
+		#    end
+		#  end
+		#end
+		#after getting the closest, get the next closest point on said bisector
+		#then get the bisectors associated with those two vertices
+		#find their closest, non-outside intersection
+		#get the new pair of unclosed/unpaired bisector/segments
+		#repeat until both unclosed are from the same bisector parent (ie the close each other)
+		#dump out the collected set as a polygon
 	end
 
 	def area
