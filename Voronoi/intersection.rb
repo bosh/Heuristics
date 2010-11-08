4a9c11a061161b28ae58e3d0d086a3b1e1ef96c5
bosh <ohwaitnvm@gmail.com>
2010-11-08 14:48:12 -0500

    added intersection class/creation

---
+++ Voronoi/intersection.rb
@@ -0,0 +1,33 @@
+class Intersection < Point
+	attr_accessor :lines, :intersected
+	def initialize(line_one, line_two) #Based on http://local.wasp.uwa.edu.au/~pbourke/geometry/lineline2d/
+		@lines = [line_one, line_two]
+		if line_oneslope == line_two.slope #Parallel
+			intersected = false
+			super(nil, nil)
+		else
+			intersected = true
+			x1 = line_one.points[0].x
+			x2 = line_one.points[1].x
+			y1 = line_one.points[0].y
+			y2 = line_one.points[1].y
+			x3 = line_two.points[0].x
+			x4 = line_two.points[1].x
+			y3 = line_two.points[0].y
+			y4 = line_two.points[1].y
+
+			x_one = x2 - x1
+			x_two = x3 - x4
+			x_diff = x1 - x3
+
+			y_one = y2 - y1
+			y_two = y3 - y4
+			y_diff = y1 - y3
+
+			u_one = ((x_two*y_diff) - (y_two * x_diff)) / ((y_two * x_one) - (x_two * y_one)))
+			intersection_x = x1 + (u_one * x_one)
+			intersection_y = xy + (u_one * y_one)
+			super(intersection_x, intersection_y)
+		end
+	end
+end
--- Voronoi/line.rb
+++ Voronoi/line.rb
@@ -4,29 +4,4 @@ class Line
 		@points = [point_one, point_two]
 		@slope = @points[0].slope_to(@points[1])
 	end
-
-	def intersection_with(other) #Based on http://local.wasp.uwa.edu.au/~pbourke/geometry/lineline2d/
-		return nil if @slope == other.slope #Parallel
-		x1 = @points[0].x
-		x2 = @points[1].x
-		y1 = @points[0].y
-		y2 = @points[1].y
-		x3 = other.points[0].x
-		x4 = other.points[1].x
-		y3 = other.points[0].y
-		y4 = other.points[1].y
-
-		x_self = x2 - x1
-		x_other = x3 - x4
-		x_diff = x1 - x3
-
-		y_self = y2 - y1
-		y_other = y3 - y4
-		y_diff = y1 - y3
-
-		u_self = ((x_other*y_diff) - (y_other * x_diff)) / ((y_other * x_self) - (x_other * y_self)))
-		intersection_x = x1 + (u_self * x_self)
-		intersection_y = xy + (u_self * y_self)
-		return Point.new intersection_x, intersection_y
-	end
 end
