ea91f54c25094f3de4666e33387ccfe7aa8ce939
bosh <ohwaitnvm@gmail.com>
2010-11-29 00:07:06 -0500

    generator works

--- Dating/generator.rb
+++ Dating/generator.rb
@@ -1,3 +1,5 @@
+require 'socket'
+
 class Person
 	attr_accessor :n, :attributes, :path
 	def initialize(n, path)
@@ -9,65 +11,51 @@ class Person
 
 	def generate_weights!
 		zeros = [((n.to_f/3) - 3 + 6*rand).to_i, 0].max
+		zeros = [zeros, n - 2].min
 		nonzeros = n - zeros
-		pos = (nonzeros/2 + rand*nonzeros/4).to_i
+		pos = [(nonzeros/2 + rand*nonzeros/4).to_i, 1].max
 		neg = nonzeros - pos
 		positives = Array.new(pos, 1.0 / pos)
-		negatives = Array.new(neg, -1.0 / pos)
-		4.times do
-			#positives
-			doubles = Array.new((2 + pos*rand).to_i)
-			doubles.map!{(pos*rand).floor}
-			halves = Array.new(doubles.size)
-			halves.map!{(pos*rand).floor}
-			doubles.each do |d|
-				if positives[d] > 0.5
-					positives[d] *= 2
-				else
-					halves.pop
-				end
-			end
-			halves.each {|h| positives[h] /= 2 }
-			#negatives
-			doubles = Array.new((2 + neg*rand).to_i)
-			doubles.map!{(neg*rand).floor}
-			halves = Array.new(doubles.size)
-			halves.map!{(neg*rand).floor}
-			doubles.each do |d|
-				if negatives[d] < -0.5
-					negatives[d] *= 2
-				else
-					halves.pop
-				end
-			end
-			halves.each {|h| negatives[h] /= 2 }
-		end
-		positive_total = positives.inject{|s,v| s += v}
-		until positive_total <= 1
-			positive_total -= positives.pop
-			zeros += 1
-		end
-		if positive_total < 1
-			positives[0] += 1 - positive_total
-		end
-		negative_total = negatives.inject{|s,v| s += v}
-		until negative_total >= -1
-			negative_total -= negatives.pop
-			zeros += 1
-		end
-		if negative_total > -1
-			negatives[0] -= -1 - negative_total
-		end
-		all = Array.new(zeros)
+		negatives = Array.new(neg, -1.0 / neg)
+		postives = skew(positives, 5, rand*0.5*positives.size)
+		negatives = skew(negatives, 5, rand*0.5*negatives.size)
+		#Round to three places
+		positives.map!{|i| ((i*1000).floor/1000.0)}
+		negatives.map!{|i| ((i*1000).floor/1000.0)}
+		#If rounding causes slight sum reduction, take that out of the first value
+		positives[0] += 1 - positives.inject{|s,v| s += v}
+		negatives[0] += -1 - negatives.inject{|s,v| s += v}
+		#reduce to three places to avoid floating point mantissa growth again
+		positives.map!{|i| ((i*1000).round/1000.0)}
+		negatives.map!{|i| ((i*1000).round/1000.0)}
+		all = Array.new(zeros, 0)
 		all += negatives
 		all += positives
-		all.sort_by{rand}
-		@attributes = all
+		puts all.join("\t")
+		@attributes = all.sort_by{rand}
+	end
+
+	def skew(ary, times = 1, num = 2, floor = -1, ceil = 1)
+		num = num.to_i
+		(0...times).each do
+			ups = []
+			downs = []
+			num.times{ ups << (ary.size*rand).floor}
+			num.times{ downs << (ary.size*rand).floor}
+			amounts = downs.map{|i| ary[i]*0.5}
+			amounts.each_with_index do |v, i|
+				amounts[i] = [amounts[i], floor - ups[i]].max
+				amounts[i] = [amounts[i], ceil + downs[i]].min
+				ary[ups[i]] += amounts[i]
+				ary[downs[i]] -= amounts[i]
+			end
+		end
+		ary
 	end
 
 	def report
-		File.new(@path, 'w') do |file|
-			file.write @attributes.join("\n")
+		File.open(@path, 'w') do |file|
+			file.print @attributes.join("\n")
 		end
 	end
 end
@@ -78,7 +66,16 @@ $host = 'localhost'
 $port = 20000
 $filepath = './person.txt'
 connection = TCPSocket.open($host, $port)
-connection.puts "Player"
-n = connection.readline.split(":")[1].to_i
-person = Person.new(n, $filepath)
-connection.puts $filepath
+connection.puts "Person"
+
+# n = nil
+# while line = connection.gets
+# 	puts line.chop
+# 	if !n
+# 		n = line.split(":")[1].to_i
+# 		person = Person.new(n, $filepath)
+ 		person = Person.new(10, $filepath)
+# 	end
+# end
+# connection.puts $filepath
+# connection.close
