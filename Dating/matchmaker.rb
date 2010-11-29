da0931e9d76cadf0b03709f98e5d5b63df59e460
bosh <ohwaitnvm@gmail.com>
2010-11-29 04:34:34 -0500

    added candidate special cases and standard generation, reduced looping in play

--- Dating/matchmaker.rb
+++ Dating/matchmaker.rb
@@ -12,34 +12,61 @@ class DatingGame
 	end
 
 	def play!
-		reading_for_score = false
 		while line = @connection.gets
 			puts line
-			if line =~ /N:\d+/ && !@n
+			if line =~ /N:\d+/ && !@n		#format: N:100
 				@n = line.split(":").last.to_i
-			elsif @candidates.size < 20
+			elsif @candidates.size < 20		#format: SCORE1:v1:v2: ... :vn
 				@candidates << PregenCandidate.new(line, @n)
-			elsif reading_for_score
-				@candidates.last.score = line.to_f
-				reading_for_score = false
-			else
+			else							#format: SCORE:0:0:0
+				@candidates.last.score = line.split(":")[1].to_f
 				c = generate_new_candidate
 				@candidates << c
 				@connection.puts c.to_submit
-				reading_for_score = true
 			end
 		end
 	end
 
 	def generate_new_candidate
-		#TODO this is the meat, innit
+		attr_ary = case candidates.size
+		when 20	#The 21st candidate should be the best candidate thus far, made into a bit vector
+			absolute_scores = candidate_scores.map{|i| i.abs}
+			best_thus_far = @candidates[absolute_scores.index(absolute_scores.max)]
+			if best_thus_far.score > 0 #customized rounding on all scores for the best known candidate thus far
+				best_thus_far.attributes.map{|v| v < 0.4 ? 0 : v >= 0.55 ? 1 : 0}
+			else #a reverse candidate should have a set of reversed scores
+				best_thus_far.attributes.map{|v| v > 0.6 ? 0 : v <= 0.45 ? 1 : 0}
+			end
+		when 21	#Wide square wave (one period)
+			Array.new((@n/2.0).floor, 1) + Array.new((@n/2.0).ceil, 1)
+		when 22	#Two period square wave
+			tmp = Array.new((@n/4.0).ceil, 1) + Array.new((@n/4.0).round, 0) + Array.new((@n/4.0).floor, 1)
+			tmp += Array.new(@n - tmp.size, 0)
+			tmp
+		when 23 #Four period square wave
+			half = Array.new((@n/8.0).ceil, 1) + Array.new((@n/8.0).round, 0) + Array.new((@n/8.0).round, 1)
+			half += Array.new((@n/2.0 - half.size).floor, 0) + half
+			half += Array.new(@n - half.size, 0)
+			half
+		when 24	#Alternating on-off attributes (N/2 period square wave) (done in 0-1-0-1 pattern as all others start with 1 and end with 0 if even length)
+			(0...@n).map{|i| i%2==0 ? 0 : 1}
+		else #After sacrificing five to crazy ideas, why not just let the optimizer work? B)
+			x = candidate_attributes_matrix
+			y = candidate_scores
+			w_star = (x.t * x).inv * y
+			w_star.to_a.flatten.map{|v| v.abs < 0.33 ? 0 : v >= 0.55 ? 1 : 0} #1 for attrs we want, anything for zeros, 0 for attrs we dont want
+		end
 	end
+
+	def candidate_scores; Matrix[@candidates.map{|c| [c.score]} end
+	def candidate_attributes_matrix; Matrix[@candidates.map(&:attributes)] end
 end
 
 class Candidate
 	attr_accessor :score, :attributes, :n
-	def initialize
-		#TODO
+	def initialize(attrs)
+		@score = nil
+		@attributes = attrs
 	end
 
 	def to_submit; @attributes.join(":") end
@@ -49,7 +76,7 @@ class PregenCandidate < Candidate
 	def initialize(line, n)
 		@n = n
 		line = line.split(":")
-		@score = line.shift.to_i
+		@score = line.shift.to_f
 		@attributes = line.map(&:to_f)
 	end
 end
