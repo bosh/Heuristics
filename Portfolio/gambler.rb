56e2e6d367a31cb1353cd77f08ebdfc345e90f3a
bosh <ohwaitnvm@gmail.com>
2010-12-06 01:13:05 -0500

    new data with valid numbers and single field classes, updated code to work with it

--- Portfolio/gambler.rb
+++ Portfolio/gambler.rb
@@ -1,8 +1,36 @@
 require 'socket'
 
+class ExpectedValue
+	attr_accessor :weights, :probabilities, :dot_values, :value
+	def initialize(weights, probs, calculate = true)
+		@weights = weights
+		@probabilities = probs
+		calculate! if calculate
+	end
+
+	def calculate!
+		@dot_values = []
+		(0...@weights.size).each{|i| @dot_values << @weights[i]*@probabilities[i]}
+		@value = @dot_values.inject{|sum,v| sum += v }
+	end
+
+	def skew_probabilities(direction)
+		if direction == :high
+			temp = @probabilities[2]
+			@probabilities[2] -= temp*0.5
+			@probabilities[0] += temp*0.5
+		elsif direction == :low
+			temp = @probabilities[0]
+			@probabilities[0] -= temp*0.5
+			@probabilities[2] += temp*0.5
+		end
+	end
+end
+
 class Gamble
-	attr_accessor :id, :returns, :probabilities, :links, :category, :history
-	def initialize(id, rh, ph, rm, pm, rl, pl)
+	attr_accessor :id, :game, :returns, :probabilities, :links, :category, :history, :category_belief_history
+	def initialize(game, id, rh, ph, rm, pm, rl, pl)
+		@game = game
 		@id = id.to_i
 		@returns = {
 			:low => rl.to_f,
@@ -16,6 +44,7 @@ class Gamble
 		}
 		@links = []
 		@category = nil
+		@category_belief_history = []
 		@history = []
 	end
 
@@ -23,15 +52,58 @@ class Gamble
 		@links.map{|l| l.gambles}.flatten.uniq - [gamble]
 	end
 
-	def decompose_system
-		#TODO
+	def fully_linked_group?
+		@game.fully_linked_system?(self)
+	end
+
+	def full_link_chain
+		@game.full_link_chain_from(self)
+	end
+
+	def expected_value(class_guess, calculate = false)
+		pre_class_ev = ExpectedValue.new([@returns[:high],[@returns[:med],[@returns[:low]], [@probabilities[:high],[@probabilities[:med],[@probabilities[:low]], calculate)
+		guessed_post_class_ev = pre_class_ev.skew_probabilities(class_guess)
+		group_size = fully_linked_group?
+		link_chain_expectations = case group_size
+		when 1
+			[0,1,0]
+		when 2
+			#h = pr this second * pr first was h |other was first
+			#m = pr this first + pr this was second * pr first was m |other was first
+			#l = pr this second * pr first was l |other was first
+			[0,1,0] #h,m,l
+		when 3
+			#h = pr this second * (pr first was h |other was others.first + pr first was h |other was others.second)
+			# 	+pr this third * (pr first two were HH (both orders))
+			#m = pr this first + pr this was second * (
+			#	pr first was others.first * pr first was m |other was others.first
+			#	+pr first was others.second pr first was m |other was others.second
+			#)	+pr this third * (pr first two were not HH or LL)
+			#l = pr this second * (pr first was l|other was others.first + pr first was l |other was others.second
+			# 	+pr this third * (pr first two were LL (both orders))
+			[0,1,0] #h,m,l
+		when 4
+			#h = pr this was first * pr this is h + pr this was second and the first was h + this was third and first two were HH (both ways) + this was fourth and first three were HHH, HHL, HHM, HLH, LHH, HMH, MHH
+			#m = pr this was first * pr this is m + pr this was second and the first was m for all firsts
+			#	+pr this was third * pr first two were not HH or LL (both orders both times)
+			#	+pr this was third * pr first three were HMM, HML, HLM, MMM, MHL, MLH, MML, MMH, MLM, MHM, LMM, LHM, LMH
+			#l = pr this was first * pr this is l + pr this was second and the first was l + this was third and first two were HH (both ways) + this was fourth and first three were LLL, LlH, LLM, LHL, HLL, LML, MLL
+			[0,1,0] #h,m,l
+		else
+			[0,1,0]
+		end
+	end
+
+	def link_chain_expectations
+		group = linked_gambles
+		probabilities = []
 	end
 end
 
 class Link
 	attr_accessor :gambles
 	def initialize(gamble_one, gamble_two)
-		@gambles = [gamble_one, gamble_two]
+		@gambles = [gamble_one.to_i, gamble_two.to_i]
 	end
 end
 
@@ -62,11 +134,10 @@ class Gambler
 				mode = :link
 			elsif mode == :gamble
 				line =~ /(\d+), (\d+\.\d+), (\d+\.\d+), (\d+\.\d+), (\d+\.\d+), (\d+\.\d+), (\d+\.\d+)/
-				@gambles << Gamble.new($1, $2, $3, $4, $5, $6)
+				@gambles << Gamble.new(self, $1, $2, $3, $4, $5, $6)
 			elsif mode == :gambleatts #This will change when category is implemented as a 0-15 number
-				line =~ /(\d+), (\d), (\d), (\d), (\d)/
-				category = 8*($2.to_i) + 4*($3.to_i) + 2*($4.to_i) + 1*($5.to_i)
-				@gambles.select{|g| g.id == $1.to_i}.first.category = category
+				line =~ /(\d+), (\d)/
+				@gambles.select{|g| g.id == $1.to_i}.first.category = $2.to_i
 			elsif mode == :link
 				line =~ /(\d+), (\d+)/
 				add_link($1.to_i, $2.to_i)
