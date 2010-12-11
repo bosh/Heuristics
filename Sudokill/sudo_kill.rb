08caf1c84947cd614f94681bfcdb48f42607d2a8
bosh <ohwaitnvm@gmail.com>
2010-12-11 03:41:17 -0500

    added turn history, board maintenance, more protocol support, timing, turn advancing

--- Sudokill/sudo_kill.rb
+++ Sudokill/sudo_kill.rb
@@ -1,7 +1,7 @@
 require 'socket'
 
 class SudoKiller
-	attr_accessor :connection, :board, :player_number, :player_count, :moves
+	attr_accessor :connection, :board, :time_taken, :player_number, :player_count, :moves, :turn
 
 	def initialize(host, port)
 		connect(host, port)
@@ -18,14 +18,23 @@ class SudoKiller
 
 	# Main loop
 	def play!
+		@moves = []
 		while line = read
-			if line =~ /START\|(\d+)\|(\d+)\|(.*)/
+			if line =~ /(READY|WAIT)/
+				# do nothing, this is just waiting
+			elsif line =~ /REJECT/
+				# we made a bad move :(
+				# if this weren't an autofail, there would need to be a move undo-er method
+			elsif line =~ /START\|(\d+)\|(\d+)\|(.*)/
+				@time_taken = 0
 				@player_number = $1.to_i
 				@player_count = $2.to_i
 				create_board!($3.chomp)
+				@moves = []
+				@turn = 0
 			elsif line =~ /\A(\d+) (\d+) (\d+) (\d+)/
-				#		   row, col, val, player
-				@moves << [$1,  $2,  $3,  $4].map(&:to_i)
+				#Idea, it would be cool to be able to run analysis on opponent moves and figure out their bias/preferences, and then spend more time computing and their expected path
+				take_move!($1.to_i, $2.to_i, $3.to_i, $4.to_i)
 			elsif line =~ /ADD\|(\d+)\|(\d+)\|(.*)/
 				# create_board!($3.chomp) #redundant if you can guarantee the deltas received give full information
 				make_move!
@@ -41,9 +50,41 @@ class SudoKiller
 		@board = text.split("|").map{|line| line.split(" ").map{|c| c.to_i} }
 	end
 
+	def advance_turn!
+		@turn += 1
+	end
+
+	def update_board!(row, col, val)
+		@board[row][col] = val
+	end
+
+	# A move reported from the server, returns a Move
+	def take_move!(row, col, number, player)
+		@moves << [row, col, number]
+		update_board!(row, col, val)
+		advance_turn!
+	end
+
+	# A move from this instance, returns a Move
 	def make_move!
-		#TODO
-		send_move(row, col, val)
+		row, col, val = nil, nil, nil
+		@time_taken += lambda{ |start|
+			@moves << move = row, col, val = make_move_for_player_and_board(@player_number, @board.clone)
+			send_move(row, col, val)
+			@time_taken += (Time.now - start)
+		}.call(Time.now)
+
+		update_board!(row, col, val) # If the server echoes your move to you, this is redundant.
+		advance_turn!
+	end
+
+	# Simulation moves for any player, returning a hash {:board, :move}
+	def make_move_for_player_and_board(number, board)	#MAJOR TODO
+		if number == @player_number
+			find_optimal_move(board, row, col)
+		else #other player's turn, simulating appropriately
+			find_available_moves(board, row, col)
+		end
 	end
 end
 
