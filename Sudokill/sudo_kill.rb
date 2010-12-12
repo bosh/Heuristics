3b6d0987cb4e481f390ec61da0e31acc500714ed
bosh <ohwaitnvm@gmail.com>
2010-12-12 17:21:02 -0500

    added board class, placement class, path traversal, option collection

--- Sudokill/sudo_kill.rb
+++ Sudokill/sudo_kill.rb
@@ -1,7 +1,7 @@
 require 'socket'
 
 class SudoKiller
-	attr_accessor :connection, :board, :time_taken, :player_number, :player_count, :moves, :turn
+	attr_accessor :connection, :current_board, :time_taken, :player_number, :player_count, :moves, :turn
 
 	def initialize(host, port)
 		connect(host, port)
@@ -26,16 +26,15 @@ class SudoKiller
 			elsif line =~ /START\|(\d+)\|(\d+)\|(.*)/
 				@moves = []
 				@time_taken = 0
-				@player_number = $1.to_i
-				@player_count = $2.to_i
-				create_board!($3.chomp)
 				@moves = []
 				@turn = 0
+				@player_number = $1.to_i
+				@player_count = $2.to_i
+				@board = Board.new(:text => $3.chomp)
 			elsif line =~ /\A(\d+) (\d+) (\d+) (\d+)/
 				#Idea, it would be cool to be able to run analysis on opponent moves and figure out their bias/preferences, and then spend more time computing and their expected path
 				take_move!($1.to_i, $2.to_i, $3.to_i, $4.to_i)
 			elsif line =~ /ADD\|(\d+)\|(\d+)\|(.*)/
-				# create_board!($3.chomp) #redundant if you can guarantee the deltas received give full information
 				make_move!
 			elsif line =~ /(WIN|LOSE)/
 				puts "I #{$1}!"
@@ -45,16 +44,12 @@ class SudoKiller
 		end
 	end
 
-	def create_board!(text)
-		@board = text.split("|").map{|line| line.split(" ").map{|c| c.to_i} }
-	end
-
 	def advance_turn!
 		@turn += 1
 	end
 
 	def update_board!(row, col, val)
-		@board[row][col] = val
+		@current_board = @current_board.place(row, col, val)
 	end
 
 	# A move reported from the server, returns a Move
@@ -84,6 +79,102 @@ class SudoKiller
 		else #other player's turn, simulating appropriately
 			find_available_moves(board, row, col)
 		end
+	end
+end
+
+class Board
+	attr_accessor :cells, :placements, :turn, :row, :col
+
+	def initialize(args = {})
+		if args.has_key? :text
+			@cells = args[:text].split("|").map{|line| line.split(" ").map{|c| c.to_i} }
+		elsif args.has_key? :cells
+			@cells = args[:cells].clone
+			if args.has_key? :move
+				@row = args[:move][:row]
+				@col = args[:move][:col]
+				@cells[@row][@col] = args[:move][:val]
+			end
+		end
+		@turn = args.has_key?[:turn] ? args[:turn] : 0
+		@placements = args[:do_placements] ? create_all_placements! : []
+	end
+
+	def create_all_placements
+		positions = if @row && @col
+			collect_limited_options
+		else
+			collect_all_options
+		end
+		#TODO: turn a set of open points into the set of options at each point
+	end
+
+	def collect_limited_options
+		options = []
+		(0..8).each do |i|
+			empty?(@row, i) ? options << [@row, i]
+			empty?(i, @col) ? options << [i, @col]
+		end
+		#TODO: avoid collecting the same point twice. This should happen where i == row or i == col
+		options
+	end
+
+	def collect_all_options
+		options = []
+		(0..8).each do |r|
+			(0..8).each do |c|
+				empty?(r, c) ? options << [r,c]
+			end
+		end
+	end
+
+	def empty?(row, col)
+		@cells[row][col] == 0
+	end
+
+	# Gets all the values in the current row, column, and cell, and finds values from 1 to 9 that are not already included in the set
+	def possible_values(row, col)
+		((1..9).to_a - (row_values(row) += col_values(col) += cell_values(row, col)).uniq).sort
+	end
+
+	# Returns the values already placed in the given row
+	def row_values(row)
+		(0..8).collect{|c| @cells[row, c]}
+	end
+
+	# Returns the values already placed in the given column
+	def col_values(col)
+		(0..8).collect{|r| @cells[r, col]}
+	end
+
+	def cell_values(row, col)
+		cell_row = row/3
+		cell_col = col/3
+		(0..8).collect{|i| @cells[cell_row + i/3][cell_col + i%3] }
+	end
+
+	# Returns the board state for a board descendant of the current board, with a certain move taken
+	def place(row, col, val)
+		if path = @placements.select{|p| p.row == row && p.col == col && p.val = val}.first
+			path.result
+		else
+			Placement.new(self, row, col, val).result
+		end
+	end
+end
+
+class Placement
+	attr_accessor :row, :col, :val, :parent, :child, :turn
+
+	def initialize(parent, row, col, val)
+		@row = row
+		@col = col
+		@val = val
+		@parent = parent
+	end
+
+	def result # Delay computation until necessary, but memoize afterwards
+		@child ||= Board.new(:cells => parent.cells, :move => {:row => @row, :col => @col, :val => @val}, :turn = @parent.turn+1)
 	end
 end
 
