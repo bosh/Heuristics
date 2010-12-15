d9934b6f7bd4e87e9e86821c3d82ce25381d0d91
Bosh <ohwaitnvm a gmail d com>
2010-12-15 18:04:12 -0500

    attempted improvements on matchmaker OLS estimations

--- Dating/matchmaker.rb
+++ Dating/matchmaker.rb
@@ -49,8 +49,6 @@ class DatingGame
 			half += Array.new([(@n/2.0 - half.size).floor, 0].max, 0) + half
 			half += Array.new([@n - half.size, 0].max, 0)
 			half
-		when 24	#Alternating on-off attributes (N/2 period square wave) (done in 0-1-0-1 pattern as all others start with 1 and end with 0 if even length)
-			(0...@n).map{|i| i%2==0 ? 0 : 1}
 		when 39 #Last attempt. OH NOES
 			puts "last attempt, baby"
 			absolute_scores = candidate_scores.to_a.flatten.map{|i| i.abs}
@@ -67,7 +65,7 @@ class DatingGame
 			x = candidate_attributes_matrix
 			y = candidate_scores
 			w_star = (x.t * x).inv * x.t * y
-			w_star.to_a.flatten.map{|v| v < 0 ? 0 : v >= 0.01 ? 1 : 0} #1 for attrs we want, anything for zeros, 0 for attrs we dont want
+			w_star.to_a.flatten.map{|v| v < 0 ? 0 : 1 } #1 for attrs we want, anything for zeros, 0 for attrs we dont want
 		end
 		Candidate.new(attr_ary)
 	end
